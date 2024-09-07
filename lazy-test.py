e50378542e1ab5756d688eba334a23390487c8da
doesntmatter <doesntmatter>
2024-09-07 13:56:16 +0100

    whatever

--- lazy-test.py
+++ lazy-test.py
@@ -3,7 +3,7 @@
 import os
 import argparse
 import subprocess
-from multiprocessing import Pool
+from multiprocessing import Pool, Manager
 import glob
 import json
 import sys,sqlite3 as lite,os,argparse,ast
@@ -13,7 +13,6 @@ try:
     import  xml.etree.cElementTree as ET
 except ImportError:
     import xml.etree.ElementTree as ET
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Usage: python3 lazy-test.py <path to file containing IPs or URLs>')
@@ -34,6 +33,7 @@ svcfolder="services"
 nmap_res={}
 
 check = False
+
 
 def db_connect():
     con = lite.connect(db)
@@ -60,18 +60,16 @@ def db_connect():
     return con
 
 def nmap_scan(ip):
-    global check
     if "/" in str(ip): 
         print('if you are looking to test an internal network this is not the way, use naptest')
         targets = ip.split('/')[0]
-        subprocess.run(['nmap','-sn','-oA',f'nmap/{targets}.hosts.up',ip])
+        result = subprocess.run(['nmap','-sn','-oA',f'nmap/{targets}.hosts.up',ip], capture_output=True, text=True)
+        for line in result.stdout.splitlines():
+            if 'Nmap scan report for' in line:
+                live_hosts.append(line.split()[-1])
 
-        print(check)
-        check = True
-        print(check)
     else:
-        print('lets say scan is done')
-        # subprocess.run(['nmap','-Pn','--min-rate=500','--max-retries=3','--max-rtt-timeout=900ms', '-T4', '-sS','-dd','-oA',f'nmap/{ip}.tcp',ip])
+        subprocess.run(['nmap','-Pn','--min-rate=500','--max-retries=3','--max-rtt-timeout=900ms', '-T4', '-sS', '-sV','-dd','-oA',f'nmap/{ip}.tcp',ip])
 
 def filehandler(arginput):
     if os.path.isfile(arginput):
@@ -527,7 +525,7 @@ def runtests():
         if key == 'closed ranges' or key == 'closed ports':
             continue
         else:
-            print(key)
+            # print(key)
             if key == 'ftp':
                 print('running ftp scan on:',servicearray[key]['tcp'])
             if key == 'http' or key == 'https':
@@ -567,7 +565,9 @@ def nuclei(host):
 if __name__ == '__main__':
     global servicearray
     servicearray = {}
+
     live_hosts = []
+
     if args.nmap:
         os.makedirs('nmap', exist_ok=True)
         file_path = args.nmap
@@ -575,18 +575,26 @@ if __name__ == '__main__':
             try:
                 with open(file_path) as file:
                     targets_list=[line.strip() for line in file]
-                    with Pool(processes=20) as pool:
-                        pool.map(nmap_scan, targets_list)
-
-                    print(check)
-                    if check:
-                        print('it run')
+                    with Manager() as manager:
+                        live_hosts = manager.list()
                         with Pool(processes=20) as pool:
-                            pool.map(nmap_scan, live_hosts)
+                            pool.map(nmap_scan, targets_list)
+
+                        print(live_hosts)
+                        if live_hosts:
+                            with Pool(processes=20) as pool:
+                                pool.map(nmap_scan, live_hosts)
+
 
 
             except FileNotFoundError:
+                print(args.nmap)
+                print(file_path)
+                print('doesnt work?')
                 print("File not found.")
+
+
+
 
         current_directory = os.getcwd()
 
@@ -608,61 +616,3 @@ if __name__ == '__main__':
         outputarray()
         outputfiles()
         runtests()
-
-
-
-    # if args.parse:
-    #     file_path = args.parse
-    #     if os.path.isfile(file_path):
-    #         try:
-    #             nmap_results = ET.ElementTree(file=f)
-    #             parse_ips(nmap_results)
-    #             if args.test:
-    #                 None
-    #             else:
-    #                 with open('nmap_open_ports.json', 'w') as f:
-    #                     json.dump(nmap_res, f)
-
-    #         except:
-    #             print("That's not a recognised file type.")
-
-    #     elif os.path.isdir(file_path):
-    #         files = glob.glob(f"{file_path}/*.xml")
-    #         if args.test:
-    #             for f in files:
-
-    #                 try:
-    #                     nmap_results = ET.ElementTree(file=f)
-    #                     parse_ips(nmap_results)   
-
-
-    #                 except Exception as e:
-    #                     print(f"Error parsing {f}: {e}")
-            
-    #             try:
-    #                 None
-    #             except:
-    #                 None
-
-    #         else:
-    #             for f in files:  
-    #                 try:
-    #                     nmap_results = ET.ElementTree(file=f)
-    #                     parse_ips(nmap_results)   
-
-                                         
-    #                 except Exception as e:
-    #                     print(f"Error parsing {f}: {e}")
-
-
-    #             with open('nmap_open_ports.json', 'w') as f:
-    #                         json.dump(nmap_res, f)
-
-                
-
-    # print(nmap_res)
-
-
-
-
-
